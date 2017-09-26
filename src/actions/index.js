f93ace7293f7a8438764b751a44ce4be83e850e2
Dylan <dylan@Dylans-MacBook.local>
2017-09-26 15:04:10 -0400

    cleaned up the actions file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,20 @@
 export const FETCH_PRS = "FETCH_PRS";
 export const PR_ERROR = "PR_ERROR";
 
+// this action fires of a reducer that changes the prError state to true
+const prError = () => {
+  return { 
+    type: PR_ERROR,
+    hasError: true
+  }
+};
 // uses redux thunk and async/await to fetch pull requests and dispatch pr reducer
 // if error is caught it will dispatch pr error reducer
-export const fetchPrs = () => async (dispatch) => {
+export const fetchPrs = () => async dispatch => {
   try {
-    const res = await
-    fetch('https://lambda-admin.herokuapp.com/pull-requests')
+    const res = await fetch('https://lambda-admin.herokuapp.com/pull-requests')
     dispatch({ type: FETCH_PRS, payload: await res.json() });
   } catch(err) {
-      dispatch({ type: PR_ERROR, hasError: true }); // could create seperate function for dispatch input as project scales
+      dispatch(prError()); // by seperating this out we can reuse the prError function to change the error state in future action creators
   };
 };
-
